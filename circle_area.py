af27c600bc2660179fafbc028395fb1f652f931e
vjnnxx <florianovinicius09@gmail.com>
2020-10-05 20:00:59 -0300

    Created a program that calculates the area of a circle

---
+++ circle_area.py
@@ -0,0 +1,6 @@
+#program that calculates the area of a circle
+import math
+PI = 3.14
+
+radius = int(input('Insert the circle radius: '))
+print('The area of the circle is: %0.2f' %(math.pow(radius,2)*PI))
